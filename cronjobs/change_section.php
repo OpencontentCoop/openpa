38b07d1b7ab27f5e2a537760cd99b16821cee12b
Luca Realdi <luca.realdi@opencontent.it>
2014-04-30 10:30:14 +0200

    Merge di change_section

--- cronjobs/change_section.php
+++ cronjobs/change_section.php
@@ -1,70 +1,290 @@
 <?php
 
-if ( $isQuiet )
+eZExtension::activateExtensions();
+
+$cli = eZCLI::instance();
+$cli->setUseStyles( true );
+$cli->setIsQuiet( $isQuiet );
+
+// Login con admin
+$user = eZUser::fetchByName( 'admin' );
+if ( $user )
+{
+    eZUser::setCurrentlyLoggedInUser( $user, $user->attribute( 'contentobject_id' ) );
+    $cli->output( "Eseguo lo script da utente {$user->attribute( 'contentobject' )->attribute( 'name' )}" );
+}
+else
 {    
-    $cli->setIsQuiet( true );
+    throw new InvalidArgumentException( "Non esiste un utente admin" ); 
 }
+$loggedUser = eZUser::currentUser();
+$cli->output( "Si sta eseguendo l'agente con l'utente " . $loggedUser->attribute( 'contentobject' )->attribute( 'name' )  );
 
-$handlers = eZINI::instance( 'walkobjects.ini' )->variable( 'WalkObjectsHandlers', 'AvaiableHandlers' );
-$params = array();
-$params[] = "Per handler params:";
-foreach( $handlers as $handler )
-{
-    $class = eZINI::instance( 'walkobjects.ini' )->variable( $handler, 'PHPClass' );
-    $params[] = $handler . ": " . $class::help();
-}
 
-$handlerName = 'change_section';
-$handler = false;
-if ( in_array( $handlerName, $handlers ) )
-{
-    if ( eZINI::instance( 'walkobjects.ini' )->hasVariable( $handlerName, 'PHPClass' ) )
+// Lettura dei file INI
+$ini = eZINI::instance( 'openpa.ini' );
+$Classes = $ini->variable( 'ChangeSection','ClassList' );
+$rootNodeIDList = $ini->variable( 'ChangeSection','RootNodeList' );
+$DataTime =  $ini->variable( 'ChangeSection','DataTime' );
+$SectionIDs =  $ini->variable( 'ChangeSection','ToSection' );
+$SectionDefault =  $ini->hasVariable( 'ChangeSection','ToSectionDefault' ) ? $ini->variable( 'ChangeSection','ToSectionDefault' ) : 0;
+$ScadenzaSecondi = $ini->hasVariable( 'ChangeSection','ScadeDopoTotSecondi' ) ? $ini->variable( 'ChangeSection','ScadeDopoTotSecondi' ) : 0;
+$UsaValore = $ini->hasVariable( 'ChangeSection','UsaValore' ) ? $ini->variable( 'ChangeSection','UsaValore' ) : false;
+$Ignora = $ini->hasVariable( 'ChangeSection','Ignora' ) ? $ini->variable( 'ChangeSection','Ignora' ) : false;
+$ScadenzaDefault = $ini->hasVariable( 'ChangeSection','ScadeDopoTotSecondiDefault' ) ? $ini->variable( 'ChangeSection','ScadeDopoTotSecondiDefault' ) : 9262300400;
+$currrentDate = time();
+
+$countClasses = count( $rootNodeIDList );
+$i = 0;
+$clones = array();   
+
+foreach( $rootNodeIDList as $class => $nodeID )
+{    
+    if ( $nodeID = 'RootNode' )
     {
-        $class = eZINI::instance( 'walkobjects.ini' )->variable( $handlerName, 'PHPClass' );
-        $handler = new $class( $options['params'] );
+        $nodeID = eZINI::instance( 'content.ini' )->variable( 'NodeSettings', 'RootNode' );
     }
-}
+    $i++;
+    $rootNode = eZContentObjectTreeNode::fetch( $nodeID );
 
-$user = eZUser::fetchByName('admin');
-eZUser::setCurrentlyLoggedInUser( $user, $user->attribute( 'contentobject_id' ) );
-
-$contentObjects = array();
-
-$count = $handler->fetchCount();
-
-$cli->output( "Number of objects to walk: $count" );
-
-$length = 50;
-$handler->setFetchParams( array( 'Offset' => 0 , 'Limit' => $length ) );
-
-$output = new ezcConsoleOutput();
-$progressBarOptions = array(
-                    'emptyChar'         => ' ',
-                    'barChar'           => '='     
-                );
-if ( $isQuiet )
-{
-    $progressBarOptions['minVerbosity'] = 10;    
-}
-$progressBar = new ezcConsoleProgressbar( $output, intval( $count ), $progressBarOptions );
-$progressBar->start();
-
-do
-{
-    $items = $handler->fetch();
+    if ( !$rootNode instanceof eZContentObjectTreeNode )
+    {
+        $cli->error( "$nodeID non trovato" );
+        continue;
+    }
     
-    foreach ( $items as $item )
-    {            
-        if ( $handler )
+    $cli->output( $i . '/' . $countClasses . ' - classe: ' . $class . ', subtree: ' . $rootNode->attribute( 'node_id' ) . ', ', false );
+    
+    $usaValore = false;
+    if ( isset( $UsaValore[$class] ) )
+    {
+        if ( $UsaValore[$class] == 'maggiore' || $UsaValore[$class] == 'minore' )
         {
-            $progressBar->advance();
-            $handler->modify( $item, $cli );
+            $usaValore = $UsaValore[$class];
+        }
+        else
+        {
+            throw new Exception( "Valore UsaValore errato: " . $UsaValore[$class] . ". Valori ammessi: maggiore minore" );
         }
     }
     
-    $handler->params['Offset'] += $length;
-} while ( count( $items ) == $length );
+    $ignora = false;
+    if ( isset( $Ignora[$class] ) )
+    {
+        if ( $Ignora[$class] == 'attributo' || $Ignora[$class] == 'secondi' )
+        {
+            $ignora = $Ignora[$class];
+        }
+        else
+        {
+            throw new Exception( "Valore Ignora errato: " . $Ignora[$class] . ". Valori ammessi: attributo secondi" );
+        }
+    }
+    
+    if ( isset( $ScadenzaSecondi[$class] ) )
+    {
+        $scadeDopoTotSecondi = $ScadenzaSecondi[$class];
+    }
+    else
+    {
+        $scadeDopoTotSecondi = $ScadenzaDefault;
+    }
+    
+    $secondiLeggibile = intval( $scadeDopoTotSecondi / 60 / 60 / 24 / 365 );
+    
+    $unpublishDateAttribute = $DataTime[$class];
+    
+    $cli->output( "attributo: $unpublishDateAttribute, secondi $scadeDopoTotSecondi ($secondiLeggibile anni)" );
+    if ( $usaValore )
+    {
+        $cli->output( "Usa il valore $usaValore" );
+    }
+    if ( $ignora )
+    {
+        $cli->output( "Ignora $ignora" );
+    }    
+    
+	if ( !$unpublishDateAttribute )
+    {
+        $cli->error( 'Attributo non trovato' );
+        continue;
+    }
+    
+    $toSection = 0;
+    
+    if ( isset( $SectionIDs[$class] ) )
+    {
+        $toSection = $SectionIDs[$class];
+    }
+    else
+    {
+        $toSection = $SectionDefault;
+    }
+    
+    if ( !is_numeric( $toSection ) )
+    {                    
+        $sectionObject = eZSection::fetchByIdentifier( $toSection, false );
+    }
+    else
+    {
+        $sectionObject = eZSection::fetch( $toSection, false );
+    }
+        
+    if ( is_array( $sectionObject ) && !empty( $sectionObject ) )
+    {                        
+        $toSection = $sectionObject['id'];
+    }
+    else
+    {
+        $cli->error( "Section $toSection non trovata" );
+        continue;
+    }    
 
-$progressBar->finish();
+	$NodeArray = $rootNode->subTree( array( 'ClassFilterType' => 'include',
+                                            'ClassFilterArray' => array( $class ),
+                                            'LoadDataMap' => false,
+                                            'Limitation' => array(),
+                                            'AttributeFilter' => array( array( 'section', '!=', $toSection ) )
+                                            )
+                                    );
+    $count = count( $NodeArray );
+    $output = new ezcConsoleOutput();
+    $progressBarOptions = array( 'emptyChar' => ' ', 'barChar'  => '=' );
+    if ( $isQuiet )
+    {
+        $progressBarOptions['minVerbosity'] = 10;    
+    }
+    $progressBar = new ezcConsoleProgressbar( $output, intval( $count ), $progressBarOptions );
+    $progressBar->start();
+    
+    foreach ( $NodeArray as $Node )
+    {
+        $progressBar->advance();
+        $Object = $Node->attribute( 'object' );         
 
-?>
+        // Debug su un nodo
+        //if ( $Node->attribute( 'node_id' ) != 668195 ){continue;}
+        //else
+        //{
+        //    var_dump( OscuraAttiHandler::isPrivacyClonedObject( $Object ) );
+        //    var_dump( $Object->attribute( 'remote_id' ) );
+        //    die();
+        //    
+        //}
+
+        $attributes = $Object->fetchAttributesByIdentifier( array( $unpublishDateAttribute ) );
+        $dateAttribute = array_shift( $attributes );
+
+        $date = $dateAttribute->content();
+        $AttributeRetractDate = $date->attribute( 'timestamp' );
+        $IniRetractDate = $Object->attribute( 'published' ) + $scadeDopoTotSecondi;
+
+        if ( $AttributeRetractDate > 0 ) 
+        {
+            $ObjectRetractDate = $AttributeRetractDate;
+        }
+        else
+        {            
+            $ObjectRetractDate = $IniRetractDate;
+        }        
+        
+        if ( !$ignora )
+        {
+            if ( $usaValore && $usaValore == 'maggiore' )
+            {
+                if ( $AttributeRetractDate > $IniRetractDate )
+                {
+                    $ObjectRetractDate = $AttributeRetractDate;
+                }
+                else
+                {
+                    $ObjectRetractDate = $IniRetractDate;
+                }
+            }
+            
+            if ( $usaValore && $usaValore == 'minore' )
+            {
+                if ( $AttributeRetractDate < $IniRetractDate )
+                {
+                    $ObjectRetractDate = $AttributeRetractDate;
+                }
+                else
+                {
+                    $ObjectRetractDate = $IniRetractDate;
+                }
+            }
+        }
+        elseif ( $ignora == 'attributo' )
+        {
+            $ObjectRetractDate = $IniRetractDate;
+        }
+        elseif ( $ignora == 'secondi' )
+        {
+            $ObjectRetractDate = $AttributeRetractDate;
+        }
+
+        if ( is_null( $dateAttribute ) )
+        {
+            $cli->error( 'Attributo non trovato' );
+            continue;
+        }
+        
+        if ( $ObjectRetractDate > 0  && $ObjectRetractDate < $currrentDate )
+        {            
+        	$isClone = false;
+            if ( OscuraAttiHandler::isPrivacyClonedObject( $Object ) )
+            {                
+                $clones[$class][] = $Object->attribute( 'main_node_id' );
+                $isClone = true;
+            }
+            elseif ( $clone = OscuraAttiHandler::hasPrivacyClonedObject( $Object ) )
+            {
+                $clones[$class][] = $clone->attribute( 'main_node_id' );
+            }
+            
+            if ( !$isClone )
+            {
+                if ( eZOperationHandler::operationIsAvailable( 'content_updatesection' ) )
+                {
+                    $operationResult = eZOperationHandler::execute( 'content',
+                                                                    'updatesection',
+                                                                    array( 'node_id'             => $Node->attribute( 'node_id' ),
+                                                                           'selected_section_id' => $toSection ),
+                                                                    null,
+                                                                    true );
+            
+                }
+                else
+                {
+                    eZContentOperationCollection::updateSection( $Node->attribute( 'node_id' ), $toSection );
+                }
+                
+                eZContentCacheManager::clearContentCacheIfNeeded( $Object->attribute( 'id' ) );
+            }
+            eZContentObject::clearCache( $Object->attribute( 'id' ) );
+            $Object->resetDataMap();
+        }
+    }
+        
+    $progressBar->finish();
+    $memoryMax = memory_get_peak_usage(); // Result is in bytes
+    $memoryMax = round( $memoryMax / 1024 / 1024, 2 ); // Convert in Megabytes
+    $cli->output( ' Memoria usata: '.$memoryMax.'M' );
+}
+
+if ( count( $clones ) > 0 )
+{
+    $cli->output();        
+            
+    foreach( $clones as $class => $cloneNodesID )
+    {                
+        foreach( $cloneNodesID as $cloneNodeID )
+        {
+            eZContentObjectTreeNode::removeSubtrees( array( $cloneNodeID ), true );
+            $memoryMax = memory_get_peak_usage(); // Result is in bytes
+            $memoryMax = round( $memoryMax / 1024 / 1024, 2 ); // Convert in Megabytes        
+            $cli->output( "Sposto nel cestino l'oggetto $class clone #$cloneNodeID (" .$memoryMax.'M)' );
+        }
+    }        
+    
+} 
--- settings/cronjob.ini.append.php
+++ settings/cronjob.ini.append.php
@@ -2,15 +2,13 @@
 [CronjobSettings]
 ExtensionDirectories[]=openpa
 
-Scripts[]=change_section.php
-Scripts[]=index_pending.php
-
 [CronjobPart-rss]
 Scripts[]=rssimport.php
 
 [CronjobPart-frequent]
 Scripts[]=clear_frontpages_cache.php
 Scripts[]=ezflowupdate.php
+Scripts[]=index_pending.php
 
 [CronjobPart-clearfrontpages]
 Scripts[]=clear_frontpages_cache.php
