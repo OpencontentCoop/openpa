bf1a0d47fe7bb2e11ca4369bce170aa635d1f8cc
Luca Realdi <luca.realdi@opencontent.it>
2014-11-06 15:23:44 +0100

    Log 1970 path Fix content_link

--- classes/services/content_link.php
+++ classes/services/content_link.php
@@ -21,18 +21,22 @@ class ObjectHandlerServiceContentLink extends ObjectHandlerServiceBase
 
     protected function getLink()
     {
-        $link = $this->container->getContentNode()->attribute( 'url_alias' );
-        if ( $this->container->currentNodeId != $this->container->currentMainNodeId
-             && $this->container->hasAttribute( 'control_area_tematica' )
-             && $this->container->attribute( 'control_area_tematica' )->attribute( 'is_area_tematica' ) )
+        $link = false;
+        if ( $this->container->getContentNode() instanceof eZContentObjectTreeNode )
         {
-            $link = $this->container->getContentObject()->attribute( 'main_node' )->attribute( 'url_alias' );
-        }
-        if ( isset( $this->container->attributesHandlers['location'] )
-             && $this->container->attributesHandlers['location']->attribute( 'contentobject_attribute' )->attribute( 'has_content' ) )
-        {
-            $link = $this->container->attributesHandlers['location']->attribute( 'contentobject_attribute' )->attribute( 'content' );
-            $this->isInternal = false;
+            $link = $this->container->getContentNode()->attribute( 'url_alias' );
+            if ( $this->container->currentNodeId != $this->container->currentMainNodeId
+                 && $this->container->hasAttribute( 'control_area_tematica' )
+                 && $this->container->attribute( 'control_area_tematica' )->attribute( 'is_area_tematica' ) )
+            {
+                $link = $this->container->getContentObject()->attribute( 'main_node' )->attribute( 'url_alias' );
+            }
+            if ( isset( $this->container->attributesHandlers['location'] )
+                 && $this->container->attributesHandlers['location']->attribute( 'contentobject_attribute' )->attribute( 'has_content' ) )
+            {
+                $link = $this->container->attributesHandlers['location']->attribute( 'contentobject_attribute' )->attribute( 'content' );
+                $this->isInternal = false;
+            }
         }
         return $link;
     }
