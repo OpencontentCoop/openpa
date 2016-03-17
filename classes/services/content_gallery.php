7cffebfebb744bef3babc95cd9808384a19ef197
Luca Realdi <lr@opencontent.it>
2016-03-17 12:50:30 +0100

    fix content gallery

--- classes/services/content_gallery.php
+++ classes/services/content_gallery.php
@@ -4,12 +4,36 @@ class ObjectHandlerServiceContentGallery extends ObjectHandlerServiceBase
 {
     protected static $cache = array();
     protected static $flipControlCache = array();
-    
+
+    protected $imagesFetchParams = array();
+
+    protected $galleriesFetchParams = array();
+
     function run()
     {
+        if ( $this->container->getContentNode() instanceof eZContentObjectTreeNode )
+        {
+            $this->imagesFetchParams = array(
+                'parent_node_id' => $this->container->currentNodeId,
+                'class_filter_type' => 'include',
+                'class_filter_array' => array( 'image' ),
+                'sort_by' => $this->container->getContentNode()->attribute( 'sort_array' )
+            );
+
+            $this->galleriesFetchParams = array(
+                'parent_node_id' => $this->container->currentNodeId,
+                'class_filter_type' => 'include',
+                'class_filter_array' => array( 'gallery' ),
+                'sort_by' => $this->container->getContentNode()->attribute( 'sort_array' )
+            );
+        }
+
         $this->fnData['has_images'] = 'getImageListCount';
+        $this->fnData['has_single_images'] = 'getSingleImagesCount';
         $this->fnData['images'] = 'getImageList';
         $this->fnData['title'] = 'getGalleryTitle';
+        $this->fnData['has_galleries'] = 'getGalleriesCount';
+        $this->fnData['galleries'] = 'getGalleryList';
     }
 
     function hasFlip()
@@ -38,124 +62,121 @@ class ObjectHandlerServiceContentGallery extends ObjectHandlerServiceBase
             {
                 self::$flipControlCache[$this->container->currentNodeId] = false;
             }
-        }        
+        }
         return self::$flipControlCache[$this->container->currentNodeId];
     }
-    
-    function getImageListCount()
-    {                
+
+    function getSingleImagesCount()
+    {
         if ( $this->hasFlip() )
         {
             return false;
         }
-        
-        if ( !isset( self::$cache[$this->container->currentNodeId] ) )
-        {            
-            $imageCount = 0;
-            $title = false;
-            
-            $fetchParams = array(
-                'parent_node_id' => $this->container->currentNodeId,
-                'class_filter_type' => 'include',
-                'class_filter_array' => array( 'image' )
-            );
+
+        if ( !$this->container->getContentNode() instanceof eZContentObjectTreeNode )
+        {
+            return false;
+        }
+
+        if ( !isset( self::$cache[$this->container->currentNodeId]['single_images_count'] ) )
+        {
             $imageCount = eZFunctionHandler::execute(
                 'content',
                 'list_count',
-                $fetchParams
+                $this->imagesFetchParams
             );
-    
-            if ( $imageCount == 0 && $this->container->currentNodeId != $this->container->currentMainNodeId )
-            {
-                $fetchParams = array(
-                    'parent_node_id' => $this->container->currentMainNodeId,
-                    'class_filter_type' => 'include',
-                    'class_filter_array' => array( 'image' )
-                );
-                $imageCount = eZFunctionHandler::execute(
-                    'content',
-                    'list_count',
-                    $fetchParams
-                );
-            }
-    
-            if ( $imageCount == 0 )
-            {            
-                $galleryChildren = eZFunctionHandler::execute(
-                    'content',
-                    'list',
-                    array(
-                         'parent_node_id' => $this->container->currentNodeId,
-                         'class_filter_type' => 'include',
-                         'class_filter_array' => array( 'gallery' ),
-                         'limit' => 1
-                    )
-                );            
-                if ( count( $galleryChildren ) > 0 && $galleryChildren[0] instanceof eZContentObjectTreeNode )
-                {
-                    
-                    $fetchParams = array(
-                        'parent_node_id' => $galleryChildren[0]->attribute( 'node_id' ),                         
-                        'class_filter_type' => 'include',
-                        'class_filter_array' => array( 'image' )
-                    );
-                    
-                    $imageCount = eZFunctionHandler::execute(
-                        'content',
-                        'list_count',
-                        $fetchParams
-                    );
-                    
-                    $title = $galleryChildren[0]->attribute( 'name' );
-                }
-            }
-            
-            self::$cache[$this->container->currentNodeId] = array(
-                'image_count' => $imageCount,
-                'fetch_params' => $fetchParams,
-                'title' => $title
-            );        
+
+            self::$cache[$this->container->currentNodeId]['single_images_count'] = $imageCount;
         }
-        return self::$cache[$this->container->currentNodeId]['image_count'] > 0;
+        return self::$cache[$this->container->currentNodeId]['single_images_count'] > 0;
     }
-    
+
+    function getGalleriesCount()
+    {
+        if ( !$this->container->getContentNode() instanceof eZContentObjectTreeNode )
+        {
+            return false;
+        }
+
+        if ( !isset( self::$cache[$this->container->currentNodeId]['galleries_count'] ) )
+        {
+            $galleryChildrenCount = eZFunctionHandler::execute(
+                'content',
+                'list_count',
+                $this->galleriesFetchParams
+            );
+            self::$cache[$this->container->currentNodeId]['galleries_count'] = $galleryChildrenCount;
+        }
+        return self::$cache[$this->container->currentNodeId]['galleries_count'] > 0;
+    }
+
+    function getImageListCount()
+    {
+        return $this->getSingleImagesCount() > 0 || $this->getGalleriesCount() > 0;
+    }
+
     function getGalleryTitle()
     {
         if ( $this->hasFlip() )
         {
             return false;
         }
-        
-        if ( isset( self::$cache[$this->container->currentNodeId]['title'] ) && self::$cache[$this->container->currentNodeId]['title'] )
-        {
-            return self::$cache[$this->container->currentNodeId]['title'];
-        }
         return 'Immagini';
     }
-    
+
     function getImageList()
-    {                        
+    {
         if ( $this->hasFlip() )
         {
             return false;
         }
-        
-        if ( !isset( self::$cache[$this->container->currentNodeId]['list'] ) )
-        {            
+
+        if ( !$this->container->getContentNode() instanceof eZContentObjectTreeNode )
+        {
+            return false;
+        }
+
+        if ( !isset( self::$cache[$this->container->currentNodeId]['single_images'] ) )
+        {
             if ( $this->getImageListCount() > 0 )
             {
-                self::$cache[$this->container->currentNodeId]['list'] = eZFunctionHandler::execute(
+                self::$cache[$this->container->currentNodeId]['single_images'] = eZFunctionHandler::execute(
                     'content',
                     'list',
-                    self::$cache[$this->container->currentNodeId]['fetch_params']
-                );                
+                    $this->imagesFetchParams
+                );
             }
             else
             {
-                self::$cache[$this->container->currentNodeId]['list'] = array();
+                self::$cache[$this->container->currentNodeId]['single_images'] = array();
             }
-        }        
-        return self::$cache[$this->container->currentNodeId]['list'];
+        }
+        return self::$cache[$this->container->currentNodeId]['single_images'];
+    }
+
+    function getGalleryList()
+    {
+        if ( !$this->container->getContentNode() instanceof eZContentObjectTreeNode )
+        {
+            return false;
+        }
+
+        if ( !isset( self::$cache[$this->container->currentNodeId]['galleries'] ) )
+        {
+            if ( $this->getGalleriesCount() > 0 )
+            {
+                self::$cache[$this->container->currentNodeId]['galleries'] = eZFunctionHandler::execute(
+                    'content',
+                    'list',
+                    $this->galleriesFetchParams
+                );
+            }
+            else
+            {
+                self::$cache[$this->container->currentNodeId]['galleries'] = array();
+            }
+        }
+        return self::$cache[$this->container->currentNodeId]['galleries'];
     }
 
 }
