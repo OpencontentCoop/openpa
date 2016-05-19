68db2b711f7995083f1d6ad8bf4d5f6baa375d84
Luca Realdi <lr@opencontent.it>
2016-05-19 14:39:29 +0200

    link check defender

---
+++ bin/php/set_url_valid.php
@@ -0,0 +1,41 @@
+<?php
+require 'autoload.php';
+
+$cli = eZCLI::instance();
+$script = eZScript::instance( array( 'description' => ( "OpenPA linkcheck defender" ),
+                                     'use-session' => false,
+                                     'use-modules' => true,
+                                     'use-extensions' => true ) );
+
+$script->startup();
+$options = $script->getOptions( "[simple][generate-csv][class:]", "",
+    array( "link_id" => "Id del link" )
+);
+$script->initialize();
+try
+{
+    /** @var eZURL[] $linkList */
+    $linkList = array();
+
+    if ($options['link_id']){
+        $link = eZURL::fetch($options['link_id']);
+        if ( $link instanceof eZURL){
+            $linkList = array($link);
+        }
+    }else{
+        $linkList = eZURL::fetchList( array( 'only_published' => true ) );
+    }
+
+    foreach ( $linkList as $link )
+    {
+        $linkID = $link->attribute( 'id' );
+        eZURL::setIsValid( $linkID, true );
+    }
+    $script->shutdown();
+}
+catch( Exception $e )
+{
+    $errCode = $e->getCode();
+    $errCode = $errCode != 0 ? $errCode : 1; // If an error has occured, script must terminate with a status other than 0
+    $script->shutdown( $errCode, $e->getMessage() );
+}
