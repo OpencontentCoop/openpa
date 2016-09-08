095e6e060f12c28aeb7996373e522aa7c034f28f
Luca Realdi <lr@opencontent.it>
2016-09-08 11:36:06 +0200

    'GestioneClassi', 'nascondi_blocco_riferimenti'

--- classes/services/content_reverse_related.php
+++ classes/services/content_reverse_related.php
@@ -2,48 +2,56 @@
 
 class ObjectHandlerServiceContentReverseRelated extends ObjectHandlerServiceBase
 {
+    protected $count;
+
     function run()
-    {        
+    {
         $this->fnData['has_data'] = 'hasData';
     }
-    
+
     protected function hasData()
     {
-        $count = 0;
+        if ($this->count === null) {
+            $this->count = 0;
 
-        if ( !in_array($this->container->currentClassIdentifier, OpenPAINI::variable( 'GestioneClassi', 'nascondi_blocco_riferimenti', array('folder') ))) {
+            if (!in_array($this->container->currentClassIdentifier,
+                OpenPAINI::variable('GestioneClassi', 'nascondi_blocco_riferimenti', array('folder')))
+            ) {
 
-            $params = array('AsObject' => false, 'LoadDataMap' => false, 'Limit' => 1);
+                $params = array('AsObject' => false, 'LoadDataMap' => false, 'Limit' => 1);
 
-            $excludeClasses = OpenPAINI::variable('GestioneClassi', 'escludi_da_riferimenti', array());
-            if (!empty( $excludeClasses )) {
-                $params['RelatedClassIdentifiers'] = array();
-                $classRepo = new \Opencontent\Opendata\Api\ClassRepository();
-                $all = $classRepo->listAll();
-                foreach ($all as $class) {
-                    if (!in_array($class['identifier'], $excludeClasses)) {
-                        $params['RelatedClassIdentifiers'][] = $class['identifier'];
+                $excludeClasses = OpenPAINI::variable('GestioneClassi', 'escludi_da_riferimenti', array());
+                if (!empty( $excludeClasses )) {
+                    $params['RelatedClassIdentifiers'] = array();
+                    $classRepo = new \Opencontent\Opendata\Api\ClassRepository();
+                    $all = $classRepo->listAll();
+                    foreach ($all as $class) {
+                        if (!in_array($class['identifier'], $excludeClasses)) {
+                            $params['RelatedClassIdentifiers'][] = $class['identifier'];
+                        }
                     }
                 }
-            }
-            if (!in_array($this->container->currentClassIdentifier,
-                OpenPAINI::variable('GestioneClassi', 'classi_che_producono_contenuti', array()))
-            ) {
-                $list = $this->container->getContentObject()->reverseRelatedObjectList(false, false, false, $params);
-                $count = count($list);
-            } elseif ($this->container->currentClassIdentifier == 'politico') {
+                if (!in_array($this->container->currentClassIdentifier,
+                    OpenPAINI::variable('GestioneClassi', 'classi_che_producono_contenuti', array()))
+                ) {
+                    $list = $this->container->getContentObject()->reverseRelatedObjectList(false, false, false,
+                        $params);
+                    $this->count = count($list);
+                } elseif ($this->container->currentClassIdentifier == 'politico') {
 
-                $params['AllRelations'] = eZContentFunctionCollection::contentobjectRelationTypeMask(array('attribute'));
-                $list = $this->container->getContentObject()->reverseRelatedObjectList(false, 'politico/membri', false,
-                    $params);
-                $count = count($list);
-            } elseif ($this->container->currentClassIdentifier == 'organo_politico') {
-                $params['AllRelations'] = eZContentFunctionCollection::contentobjectRelationTypeMask(array('attribute'));
-                $list = $this->container->getContentObject()->reverseRelatedObjectList(false,
-                    'gemellaggio/circoscrizione', false, $params);
-                $count = count($list);
+                    $params['AllRelations'] = eZContentFunctionCollection::contentobjectRelationTypeMask(array('attribute'));
+                    $list = $this->container->getContentObject()->reverseRelatedObjectList(false, 'politico/membri',
+                        false,
+                        $params);
+                    $this->count = count($list);
+                } elseif ($this->container->currentClassIdentifier == 'organo_politico') {
+                    $params['AllRelations'] = eZContentFunctionCollection::contentobjectRelationTypeMask(array('attribute'));
+                    $list = $this->container->getContentObject()->reverseRelatedObjectList(false,
+                        'gemellaggio/circoscrizione', false, $params);
+                    $this->count = count($list);
+                }
             }
         }
-        return $count > 0;
+        return $this->count > 0;
     }
 }
