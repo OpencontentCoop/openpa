240d3dde1b31439444b363da1d7df9bb9fafb4ea
Luca Realdi <lr@opencontent.it>
2017-08-01 17:21:52 +0200

    custom xmltag percorso openstreet map

--- settings/content.ini.append.php
+++ settings/content.ini.append.php
@@ -5,4 +5,11 @@ VersionHistoryClass[survey]=1000
 [link]
 AvailableClasses[]=download
 
+[CustomTagSettings]
+AvailableCustomTags[]=percorso_osm
+
+[percorso_osm]
+CustomAttributes[]=title
+CustomAttributes[]=xml_link
+
 */ ?>
