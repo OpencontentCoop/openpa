a1ae30182083c064f30ba0e7d2d9ce464c7b3a3a
Luca Realdi <lr@opencontent.it>
2020-06-10 12:49:33 +0200

    Corregge un bug nella definizione degli handler dei blocchi

--- classes/handlers/block/lista_manuale.php
+++ classes/handlers/block/lista_manuale.php
@@ -2,11 +2,18 @@
 
 class BlockHandlerListaManuale extends OpenPABlockHandler
 {
+    private static $cache = [];
 
     protected function run()
     {
-        $this->data['fetch_parameters'] = array();
-        $this->data['content'] = $this->currentBlock->attribute( 'valid_nodes' );
-        $this->data['root_node'] = ( $this->data['content'][0] instanceof eZContentObjectTreeNode ) ? $this->data['content'][0]->attribute( 'parent' ) : null;
+        if (!isset(self::$cache[$this->currentBlock->id()])) {
+            $this->data['fetch_parameters'] = array();
+            $this->data['content'] = $this->currentBlock->attribute('valid_nodes');
+            $this->data['has_content'] = count($this->data['content']) > 0;
+            $this->data['root_node'] = ($this->data['content'][0] instanceof eZContentObjectTreeNode) ? $this->data['content'][0]->attribute('parent') : null;
+            self::$cache[$this->currentBlock->id()] = $this->data;
+        } else {
+            $this->data = self::$cache[$this->currentBlock->id()];
+        }
     }
 }
