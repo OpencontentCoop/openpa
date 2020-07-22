a2c88c0e6437a49e55fbedd34b849b53c31399be
Luca Realdi <lr@opencontent.it>
2020-07-22 19:06:26 +0200

    Corregge un bug in openpa/object

--- modules/openpa/object.php
+++ modules/openpa/object.php
@@ -6,25 +6,21 @@ $objectID = $Params['ObjectID'];
 $node = false;
 $redirect = '/';
 
-if ( $objectID )
-{
-    $node = eZContentObjectTreeNode::findMainNode( $objectID, true );
-    if ( $node instanceof eZContentObjectTreeNode )
-    {
-        $redirect = $node->attribute( 'url_alias' );
-    }
-    else
-    {
-        $object = eZContentObject::fetchByRemoteID( $objectID );
-        if ( $object instanceof eZContentObject )
-        {
-            $node = $object->attribute( 'main_node' );
-            if ( $node instanceof eZContentObjectTreeNode )
-            {
-                $redirect = $node->attribute( 'url_alias' );        
+if ($objectID) {
+    if (is_numeric($objectID)) {
+        $node = eZContentObjectTreeNode::findMainNode($objectID, true);
+        if ($node instanceof eZContentObjectTreeNode) {
+            $redirect = $node->attribute('url_alias');
+        }
+    } else {
+        $object = eZContentObject::fetchByRemoteID($objectID);
+        if ($object instanceof eZContentObject) {
+            $node = $object->attribute('main_node');
+            if ($node instanceof eZContentObjectTreeNode) {
+                $redirect = $node->attribute('url_alias');
             }
         }
     }
 }
 
-return $module->redirectTo( $redirect );
+return $module->redirectTo($redirect);
