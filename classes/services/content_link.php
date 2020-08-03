89ba100d2aac4da3aef375da95756a2224812414
Luca Realdi <lr@opencontent.it>
2020-08-03 09:25:35 +0200

    Permette a content_link di riconoscere se un link è al nodo o a un nodo/link esterno

--- classes/services/content_link.php
+++ classes/services/content_link.php
@@ -1,9 +1,12 @@
 <?php
 
-
 class ObjectHandlerServiceContentLink extends ObjectHandlerServiceBase
 {
     protected $isInternal = true;
+
+    protected $isNodeLink = true;
+
+    protected $link;
 
     function run()
     {
@@ -11,6 +14,7 @@ class ObjectHandlerServiceContentLink extends ObjectHandlerServiceBase
         $this->fnData['is_internal'] = 'isInternal';
         $this->fnData['target'] = 'getTarget';
         $this->fnData['full_link'] = 'getFullLink';
+        $this->fnData['is_node_link'] = 'isNodeLink';
     }
 
     protected function getFullLink()
@@ -29,59 +33,72 @@ class ObjectHandlerServiceContentLink extends ObjectHandlerServiceBase
         return $this->isInternal;
     }
 
+    protected function isNodeLink()
+    {
+        $this->getLink();
+        return $this->isNodeLink;
+    }
+
     protected function getLink()
     {
-        $link = false;
-        if ( $this->container->getContentNode() instanceof eZContentObjectTreeNode )
-        {
-            $link = $this->container->getContentNode()->attribute( 'url_alias' );            
-            if ( $this->container->currentNodeId != $this->container->currentMainNodeId
-                 && in_array( $this->container->getContentNode()->attribute( 'class_identifier' ), OpenPAINI::variable( 'AreeTematiche', 'IdentificatoreAreaTematica', array( 'area_tematica' ) ) ) )
+        if ($this->link === null){
+            $link = false;
+            if ( $this->container->getContentNode() instanceof eZContentObjectTreeNode )
             {
-                $link = $this->container->getContentObject()->attribute( 'main_node' )->attribute( 'url_alias' );
-            }
-            if ( isset( $this->container->attributesHandlers['location'] )
-                 && $this->container->attributesHandlers['location']->attribute( 'contentobject_attribute' )->attribute( 'data_type_string' ) == eZURLType::DATA_TYPE_STRING
-                 && $this->container->attributesHandlers['location']->attribute( 'contentobject_attribute' )->attribute( 'has_content' ) )
-            {
-                $link = $this->container->attributesHandlers['location']->attribute( 'contentobject_attribute' )->attribute( 'content' );
-                $this->isInternal = false;
-            }
-            if ( isset( $this->container->attributesHandlers['internal_location'] )
-                 && $this->container->attributesHandlers['internal_location']->attribute( 'contentobject_attribute' )->attribute( 'has_content' ) )
-            {
-                $content = $this->container->attributesHandlers['internal_location']->attribute( 'contentobject_attribute' )->attribute( 'content' );
-                if ( $this->container->attributesHandlers['internal_location']->attribute( 'contentobject_attribute' )->attribute( 'data_type_string' ) == eZObjectRelationListType::DATA_TYPE_STRING )
+                $link = $this->container->getContentNode()->attribute( 'url_alias' );
+                if ( $this->container->currentNodeId != $this->container->currentMainNodeId
+                    && in_array( $this->container->getContentNode()->attribute( 'class_identifier' ), OpenPAINI::variable( 'AreeTematiche', 'IdentificatoreAreaTematica', array( 'area_tematica' ) ) ) )
                 {
-                    foreach( $content['relation_list'] as $relation )
+                    $link = $this->container->getContentObject()->attribute( 'main_node' )->attribute( 'url_alias' );
+                }
+                if ( isset( $this->container->attributesHandlers['location'] )
+                    && $this->container->attributesHandlers['location']->attribute( 'contentobject_attribute' )->attribute( 'data_type_string' ) == eZURLType::DATA_TYPE_STRING
+                    && $this->container->attributesHandlers['location']->attribute( 'contentobject_attribute' )->attribute( 'has_content' ) )
+                {
+                    $link = $this->container->attributesHandlers['location']->attribute( 'contentobject_attribute' )->attribute( 'content' );
+                    $this->isInternal = false;
+                    $this->isNodeLink = false;
+                }
+                if ( isset( $this->container->attributesHandlers['internal_location'] )
+                    && $this->container->attributesHandlers['internal_location']->attribute( 'contentobject_attribute' )->attribute( 'has_content' ) )
+                {
+                    $content = $this->container->attributesHandlers['internal_location']->attribute( 'contentobject_attribute' )->attribute( 'content' );
+                    if ( $this->container->attributesHandlers['internal_location']->attribute( 'contentobject_attribute' )->attribute( 'data_type_string' ) == eZObjectRelationListType::DATA_TYPE_STRING )
                     {
-                        $object = eZContentObject::fetch( $relation['contentobject_id'] );
-                        if ( $object instanceof eZContentObject )
+                        foreach( $content['relation_list'] as $relation )
                         {
-                            $node = $object->attribute( 'main_node' );
-                            if ( $node instanceof eZContentObjectTreeNode )
+                            $object = eZContentObject::fetch( $relation['contentobject_id'] );
+                            if ( $object instanceof eZContentObject )
                             {
-                                $link = $node->attribute( 'url_alias' );
-                                $this->isInternal = true;
+                                $node = $object->attribute( 'main_node' );
+                                if ( $node instanceof eZContentObjectTreeNode )
+                                {
+                                    $link = $node->attribute( 'url_alias' );
+                                    $this->isInternal = true;
+                                    $this->isNodeLink = false;
+                                }
                             }
                         }
                     }
-                }
-                elseif ( $this->container->attributesHandlers['internal_location']->attribute( 'contentobject_attribute' )->attribute( 'data_type_string' ) == eZObjectRelationType::DATA_TYPE_STRING )
-                {
-                    $link = $content->attribute( 'main_node' )->attribute( 'url_alias' );
-                    $this->isInternal = true;
+                    elseif ( $this->container->attributesHandlers['internal_location']->attribute( 'contentobject_attribute' )->attribute( 'data_type_string' ) == eZObjectRelationType::DATA_TYPE_STRING )
+                    {
+                        $link = $content->attribute( 'main_node' )->attribute( 'url_alias' );
+                        $this->isInternal = true;
+                        $this->isNodeLink = false;
+                    }
                 }
             }
+            $this->link = $link;
         }
-        return $link;
+        
+        return $this->link;
     }
 
     protected function getTarget()
     {
         $target = false;
         if ( isset( $this->container->attributesHandlers['open_in_new_window'] )
-             && $this->container->attributesHandlers['open_in_new_window']->attribute( 'contentobject_attribute' )->attribute( 'data_int' ) == 1 )
+            && $this->container->attributesHandlers['open_in_new_window']->attribute( 'contentobject_attribute' )->attribute( 'data_int' ) == 1 )
         {
             $target = '_blank';
         }
