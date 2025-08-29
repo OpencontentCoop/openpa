90e8cb989e82db0fb86e7782958961ff1eb13d82
Luca Realdi <luca.realdi@opencontent.it>
2025-08-29 11:35:28 +0200

    Forza la copia dell'oggetto solo per una lingua corrente

--- classes/openpaobjecttools.php
+++ classes/openpaobjecttools.php
@@ -56,95 +56,88 @@ class OpenPAObjectTools
      * @throws Exception
      * @return eZContentObject
      */
-    public static function copyObject( eZContentObject $object, $allVersions = false, $newParentNodeID = null )
+    public static function copyObject(eZContentObject $object, $allVersions = false, $newParentNodeID = null): eZContentObject
     {
         $db = eZDB::instance();
-        $db->setErrorHandling( eZDB::ERROR_HANDLING_EXCEPTIONS );
-        
-        if ( !$object instanceof eZContentObject )
-            throw new InvalidArgumentException( 'Object not found' );
-        
-        if ( !$newParentNodeID )
-            $newParentNodeID = $object->attribute( 'main_parent_node_id' );
-            
-    
+        $db->setErrorHandling(eZDB::ERROR_HANDLING_EXCEPTIONS);
+
+        if (!$object instanceof eZContentObject) {
+            throw new InvalidArgumentException('Object not found');
+        }
+
+        if (!$newParentNodeID) {
+            $newParentNodeID = $object->attribute('main_parent_node_id');
+        }
+
         // check if we can create node under the specified parent node
-        if( ( $newParentNode = eZContentObjectTreeNode::fetch( $newParentNodeID ) ) === null )
-        {
-            throw new InvalidArgumentException( 'Parent node not found' );
+        if (($newParentNode = eZContentObjectTreeNode::fetch($newParentNodeID)) === null) {
+            throw new InvalidArgumentException('Parent node not found');
         }
-    
+
         $classID = $object->attribute('contentclass_id');
-    
-        if ( !$newParentNode->attribute( 'object' )->checkAccess( 'create', $classID ) )
-        {
-            $objectID = $object->attribute( 'id' );
-            throw new Exception( "Cannot copy object $objectID to node $newParentNodeID, the current user does not have create permission for class ID $classID " );            
+
+        if (!$newParentNode->attribute('object')->checkAccess('create', $classID)) {
+            $objectID = $object->attribute('id');
+            throw new Exception(
+                "Cannot copy object $objectID to node $newParentNodeID, the current user does not have create permission for class ID $classID "
+            );
         }
-    
+
         $db = eZDB::instance();
         $db->begin();
-        try
-        {
-            $newObject = $object->copy( $allVersions );
-        }
-        catch( eZDBException $e )
-        {            
+        try {
+            $newObject = self::doCopyObject($object, eZLocale::currentLocaleCode());
+        } catch (eZDBException $e) {
             $db->rollback();
-            throw new InvalidArgumentException( $e->getMessage() );
+            throw new InvalidArgumentException($e->getMessage());
         }
         // We should reset section that will be updated in updateSectionID().
         // If sectionID is 0 then the object has been newly created
-        $newObject->setAttribute( 'section_id', $object->attribute( 'section_id' ) );
+        $newObject->setAttribute('section_id', $object->attribute('section_id'));
         $newObject->store();
-    
-        $curVersion        = $newObject->attribute( 'current_version' );
-        $curVersionObject  = $newObject->attribute( 'current' );
-        $newObjAssignments = $curVersionObject->attribute( 'node_assignments' );
-        unset( $curVersionObject );
-    
+
+        $curVersion = $newObject->attribute('current_version');
+        $curVersionObject = $newObject->attribute('current');
+        $newObjAssignments = $curVersionObject->attribute('node_assignments');
+        unset($curVersionObject);
+
         // remove old node assignments
-        foreach( $newObjAssignments as $assignment )
-        {
+        foreach ($newObjAssignments as $assignment) {
             /** @var eZNodeAssignment $assignment */
             $assignment->purge();
         }
-    
+
         // and create a new one
-        $nodeAssignment = eZNodeAssignment::create( array(
-                                                         'contentobject_id' => $newObject->attribute( 'id' ),
-                                                         'contentobject_version' => $curVersion,
-                                                         'parent_node' => $newParentNodeID,
-                                                         'is_main' => 1
-                                                         ) );
+        $nodeAssignment = eZNodeAssignment::create([
+            'contentobject_id' => $newObject->attribute('id'),
+            'contentobject_version' => $curVersion,
+            'parent_node' => $newParentNodeID,
+            'is_main' => 1,
+        ]);
         $nodeAssignment->store();
-        
+
         // fix images onPublish
-        $dataMap = $object->attribute( 'data_map' );
-        $newDataMap = $newObject->attribute( 'data_map' );
-        foreach( $newDataMap as $identifier => $attribute )
-        {
-            if ( $attribute instanceof eZContentObjectAttribute )
-            {
-                if ( $attribute->attribute( 'data_type_string' ) == 'ezimage' )
-                {
-                    $attribute->setAttribute( "data_text", '' );
-                    if ( isset( $dataMap[$identifier] ) )
-                    {
+        $dataMap = $object->attribute('data_map');
+        $newDataMap = $newObject->attribute('data_map');
+        foreach ($newDataMap as $identifier => $attribute) {
+            if ($attribute instanceof eZContentObjectAttribute) {
+                if ($attribute->attribute('data_type_string') == 'ezimage') {
+                    $attribute->setAttribute("data_text", '');
+                    if (isset($dataMap[$identifier])) {
                         $string = $dataMap[$identifier]->toString();
-                        $delimiterPos = strpos( $string, '|' );
-                        if ( $delimiterPos === false )
-                        {
+                        $delimiterPos = strpos($string, '|');
+                        if ($delimiterPos === false) {
                             $filepath = $string;
                             $alternativeText = '';
-                        }
-                        else
-                        {
-                            $filepath = substr( $string, 0, $delimiterPos );
-                            $alternativeText = substr( $string, $delimiterPos + 1 );
+                        } else {
+                            $filepath = substr($string, 0, $delimiterPos);
+                            $alternativeText = substr($string, $delimiterPos + 1);
                         }
                         $tempFilename = basename($filepath);
-                        $tempDirectory = eZSys::cacheDirectory() . '/' . eZINI::instance( 'image.ini' )->variable( 'FileSettings', 'TemporaryDir' );
+                        $tempDirectory = eZSys::cacheDirectory() . '/' . eZINI::instance('image.ini')->variable(
+                                'FileSettings',
+                                'TemporaryDir'
+                            );
 
                         $tempFile = eZFile::create(
                             $tempFilename,
@@ -160,8 +153,97 @@ class OpenPAObjectTools
                 }
             }
         }
-    
+
         $db->commit();
         return $newObject;
+    }
+
+    private static function doCopyObject(eZContentObject $object, string $languageCode = 'ita-IT'): eZContentObject
+    {
+        $user = eZUser::currentUser();
+        $userID = $user->attribute('contentobject_id');
+
+        $contentObject = clone $object;
+        $translationList = $contentObject->translationStringList();
+
+        if ($languageCode && !in_array($languageCode, $translationList)) {
+            throw new InvalidArgumentException("Language code $languageCode not found in translation list");
+        }
+
+        $contentObject->setAttribute('current_version', 1);
+        $contentObject->setAttribute('owner_id', $userID);
+
+        $contentObject->setAttribute('remote_id', eZRemoteIdUtility::generate('object'));
+
+        $db = eZDB::instance();
+        $db->begin();
+        $contentObject->store();
+
+        $originalObjectID = $object->attribute('id');
+        $contentObjectID = $contentObject->attribute('id');
+
+        $db->query(
+            "INSERT INTO ezcobj_state_link (contentobject_state_id, contentobject_id)
+                     SELECT contentobject_state_id, $contentObjectID FROM ezcobj_state_link WHERE contentobject_id = $originalObjectID"
+        );
+
+        $contentObject->setName($object->attribute('name'));
+
+        $versionList = [];
+        $versionList[1] = $object->currentVersion();
+
+        foreach ($versionList as $versionNumber => $currentContentObjectVersion) {
+            $currentVersionNumber = $currentContentObjectVersion->attribute('version');
+            $contentObject->setName($currentContentObjectVersion->name(), $versionNumber);
+            $contentObject->setName(
+                $currentContentObjectVersion->name(false, $languageCode),
+                $versionNumber,
+                $languageCode
+            );
+
+            $contentObjectVersion = $object->copyVersion(
+                $contentObject,
+                $currentContentObjectVersion,
+                $versionNumber,
+                $contentObject->attribute('id'),
+                false,
+                $languageCode,
+                $languageCode
+            );
+
+            if ($currentVersionNumber == $object->attribute('current_version')) {
+                $parentMap = [];
+                $copiedNodeAssignmentList = $contentObjectVersion->attribute('node_assignments');
+                foreach ($copiedNodeAssignmentList as $copiedNodeAssignment) {
+                    $parentMap[$copiedNodeAssignment->attribute('parent_node')] = $copiedNodeAssignment;
+                }
+                // Create node-assignment from all current published nodes
+                $nodes = $object->assignedNodes();
+                foreach ($nodes as $node) {
+                    $remoteID = eZRemoteIdUtility::generate('object');
+                    // Remove assignments which conflicts with existing nodes, but keep remote_id
+                    if (isset($parentMap[$node->attribute('parent_node_id')])) {
+                        $copiedNodeAssignment = $parentMap[$node->attribute('parent_node_id')];
+                        unset($parentMap[$node->attribute('parent_node_id')]);
+                        $remoteID = $copiedNodeAssignment->attribute('remote_id');
+                        $copiedNodeAssignment->purge();
+                    }
+                    $contentObjectVersion->assignToNode(
+                        $node->attribute('parent_node_id'),
+                        $node->attribute('is_main'),
+                        0,
+                        $node->attribute('sort_field'),
+                        $node->attribute('sort_order'),
+                        $remoteID
+                    );
+                }
+            }
+        }
+
+        $contentObject->setAttribute( 'status', eZContentObject::STATUS_DRAFT );
+        $contentObject->store();
+
+        $db->commit();
+        return $contentObject;
     }
 }
